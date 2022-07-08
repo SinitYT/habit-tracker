dda2ebba8222ffd597348c4c15c92bf9cd55dd5c
SinitYT <sinityonas@outlook.com>
2022-07-08 23:09:14 +0000

    Add event listener that would call the to be made function of saving user input data

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,27 @@
+
+
+document.addEventListener("DOMContentLoaded",function(){
+    let userData = document.getElementById('userInputT');
+    for (let ud of userData) {
+        if (this.getAttribute("type") === "text"){
+            saveHabit();
+        } else {
+            let dataType = this.getAttribute("type");
+            goalCounter(dataType);
+        
+    }
+
+
+})
+
+
+
+
 function getInputFromTextBox() {
-    var input = document.getElementById("userInputT").value;
+    let input = document.getElementById("userInputT").value[0];
     alert(input);
+    let show= document.getElementById("userInputT")[0].innerHTML= input;
+    return show;
 }
 let here= getInputFromTextBox();
 console.log(here);
